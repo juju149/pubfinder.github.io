95258d2c7b6252bb272edf990ab66711293ed445
juju149 <82048901+juju149@users.noreply.github.com>
2021-11-15 19:22:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -24,3 +24,31 @@ function initMap() {
       );
     }
 }
+
+
+const currentTheme = localStorage.getItem('theme');
+window.onload=function(){
+  const toggleSwitch = document.querySelector('.theme-switch');
+  toggleSwitch.addEventListener('change', switchTheme, false);
+}
+
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+
